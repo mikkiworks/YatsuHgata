2f6fccc0052b2a9f875b60c67d5ca04b3d9394df
YUBUKI <contact@yubukiworks.com>
2026-08-01 17:23:21 +0900

    細かい修正対応

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-		const toggleBtn = document.getElementById('js-toggle');
-		const sidebar = document.querySelector('.sidebar');
+	const toggleBtn = document.getElementById('js-toggle');
+	const sidebar = document.querySelector('.sidebar');
 
-		if (toggleBtn && sidebar) {
-			toggleBtn.addEventListener('click', () => {
-				sidebar.classList.toggle('is-open');
-			});
-		}
-	});
+	if (toggleBtn && sidebar) {
+		toggleBtn.addEventListener('click', () => {
+			const isOpen = sidebar.classList.toggle('is-open');
+			toggleBtn.setAttribute('aria-expanded', String(isOpen));
+		});
+	}
+});
